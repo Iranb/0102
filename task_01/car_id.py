b7a4020a2d87fc217d9df20405736f1e0e34b2ab
Laloauren <1060192591@qq.com>
2023-09-21 13:18:12 +0800

    task01

--- task_01/car_id.py
+++ task_01/car_id.py
@@ -1,11 +1,14 @@
 import cv2
+import numpy as np
 import regex
 import hyperlpr3 as lpr3
 from tqdm import tqdm
 from pathlib import Path
 from more_itertools import first_true
+from PIL import ImageFont, ImageDraw, Image
 
 pattern_str = '([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领]{1}[A-Z]{1}(([A-HJ-NP-Z0-9]{5}[DF]{1})|([DF]{1}[A-HJ-NP-Z0-9]{5})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳]{1})'
+
 
 def compute_acc(predcition, label):
     """
@@ -19,8 +22,9 @@ def compute_acc(predcition, label):
             correct += 1
         else:
             error_list.append(label[index])
-        sum +=1
+        sum += 1
     return correct / sum,  error_list
+
 
 def get_label(img_path, pattern_str):
     """
@@ -32,20 +36,68 @@ def get_label(img_path, pattern_str):
         label = first_true(label)
     return label
 
-def get_prediction(img_data, catcher):
+
+def get_prediction(image_data, img_data, catcher):
     prediction = catcher(img_data)
     if not prediction:
         return None
     else:
         while type(prediction) != str:
+            box = prediction[-1]
             prediction = first_true(prediction)
+        prediction = post_process(img_data, prediction)
+        save_image(image_data, img_data, prediction, box)
         return prediction
 
+
+def post_process(img_data, prediction):
+    """
+        后处理流程
+    """
+    if len(prediction) > 7:
+        gray_img = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+        gray_img = cv2.cvtColor(gray_img, cv2.COLOR_GRAY2BGR)
+        prediction = catcher(gray_img)
+        if not prediction:
+            return None
+        else:
+            while type(prediction) != str:
+                prediction = first_true(prediction)
+    if len(prediction) > 8:
+        count = 0
+        index = -1
+        for char in prediction:
+            if count >= 2:
+                break
+            if ord(char) > 127:
+                count += 1
+            index += 1
+        prediction = prediction[:index]
+    return prediction
+
+
+def save_image(image_data, img_data, prediction, box):
+    out_path = 'output/'
+    Path(out_path).mkdir(parents=True, exist_ok=True)
+    if prediction:
+        image_name = out_path + '_' + prediction + '_' + image_data.name
+
+        size = 50
+        img = Image.fromarray(img_data)
+        font = ImageFont.truetype("simfang.ttf", size, encoding="utf-8")
+        draw = ImageDraw.Draw(img)
+        width = draw.textlength(prediction, font=font)
+        draw.rectangle(((box[0], box[1] - size), (box[0] + width, box[1])), fill=(0, 0, 0))
+        draw.text((box[0], box[1] - size), prediction, font=font, fill=(255, 255, 255))
+        img1 = np.array(img)
+        cv2.imencode('.jpg', img1)[1].tofile(image_name)
+
+    return None
 
 
 if __name__ == "__main__":
 
-    data_root = '/home/hyq/code/comp/dataset/base'
+    data_root = '../../dataset/base/'
     prediction_all = []
     label_all = []
     # 初始化识别模型
@@ -58,15 +110,13 @@ if __name__ == "__main__":
             img_path=img_path, 
             pattern_str=pattern_str
         )
-        img_data = cv2.imread(str(img_path))
-        prediction = get_prediction(img_data=img_data, catcher=catcher)
+        file = str(img_path).encode('utf-8')
+        img_data = cv2.imdecode(np.fromfile(file, dtype=np.uint8), cv2.IMREAD_COLOR)
+        prediction = get_prediction(image_data=img_path, img_data=img_data, catcher=catcher)
 
         prediction_all.append(prediction)
         label_all.append(label)
 
     acc, error_list = compute_acc(predcition=prediction_all, label=label_all)
-    print(acc) # 0.953
+    print(acc)  # 0.953
     print(error_list) 
-
-
-
--- task_01/main.py
+++ task_01/main.py
@@ -1,9 +1,11 @@
 import cv2
+import numpy as np
 import regex
 import hyperlpr3 as lpr3
 from tqdm import tqdm
 from pathlib import Path
 from more_itertools import first_true
+from PIL import ImageFont, ImageDraw, Image
 
 
 class CarLicenseDetector(object):
@@ -11,16 +13,18 @@ class CarLicenseDetector(object):
     def __init__(self) -> None:
         self.catcher = lpr3.LicensePlateCatcher(detect_level=1)
 
-    def get_prediction(self, img_data):
+    def get_prediction(self, image_data, img_data):
         prediction = self.catcher(img_data)
         if not prediction:
             return None
         else:
             while type(prediction) != str:
+                box = prediction[-1]
                 prediction = first_true(prediction)
-
+            prediction = self.post_process(img_data, prediction)
+            self.save_image(image_data, img_data, prediction, box)
             return prediction
-    
+
     def preprocess(self, img_data):
         """
             TODO: 预处理阶段解决已知问题
@@ -36,26 +40,63 @@ class CarLicenseDetector(object):
         """
 
         if type(image_data) == str:
-            img_data = cv2.imread(image_data)
-        elif type(img_data) == cv2.Mat:
+            file = image_data.encode('utf-8')
+            img_data = cv2.imdecode(np.fromfile(file, dtype=np.uint8), cv2.IMREAD_COLOR)
+        elif type(image_data) == cv2.Mat:
             img_data = image_data
-        else: 
-            raise TypeError(f'Input type of {type(img_data)} not support')
-        
+        else:
+            raise TypeError(f'Input type of {type(image_data)} not support')
+
         img_data = self.preprocess(img_data)
-        prediction = self.get_prediction(img_data=img_data)
-        prediction = self.post_process(prediction)
+        prediction = self.get_prediction(image_data, img_data)
 
         return prediction
-    
-    def post_process(self, prediction):
+
+    def post_process(self, img_data, prediction):
         """
             后处理流程
         """
+        if len(prediction) > 7:
+            gray_img = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+            gray_img = cv2.cvtColor(gray_img, cv2.COLOR_GRAY2BGR)
+            prediction = self.catcher(gray_img)
+            if not prediction:
+                return None
+            else:
+                while type(prediction) != str:
+                    prediction = first_true(prediction)
+        if len(prediction) > 8:
+            count = 0
+            index = -1
+            for char in prediction:
+                if count >= 2:
+                    break
+                if ord(char) > 127:
+                    count += 1
+                index += 1
+            prediction = prediction[:index]
         return prediction
-    
-if __name__ == '__main__':
 
+    def save_image(self, image_data, img_data, prediction, box):
+        out_path = 'output/'
+        Path(out_path).mkdir(parents=True, exist_ok=True)
+        if prediction:
+            image_name = out_path + '_' + prediction + '_' + image_data.split('/')[-1]
+
+            size = 50
+            img = Image.fromarray(img_data)
+            font = ImageFont.truetype("simfang.ttf", size, encoding="utf-8")
+            draw = ImageDraw.Draw(img)
+            width = draw.textlength(prediction, font=font)
+            draw.rectangle(((box[0], box[1] - size), (box[0] + width, box[1])), fill=(0, 0, 0))
+            draw.text((box[0], box[1] - size), prediction, font=font, fill=(255, 255, 255))
+            img1 = np.array(img)
+            cv2.imencode('.jpg', img1)[1].tofile(image_name)
+
+        return None
+
+
+if __name__ == '__main__':
     detector = CarLicenseDetector()
-    result = detector.detect_single('/home/hyq/code/comp/dataset/base/单黄/_单行黄牌_豫P95085_201803221743463736022_.jpg')
+    result = detector.detect_single('../../dataset/base/单黄/_单行黄牌_y_鄂H0C650_2018032311021843417605_.jpg')
     print(result)
